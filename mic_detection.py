525ea9b14e1317cb7281685b7173cdba8a31d627
TapanDangi <tapandangi17@gmail.com>
2023-05-17 12:00:45 +0530

    17/5 commit

---
+++ mic_detection.py
@@ -0,0 +1,19 @@
+import pyttsx3
+import speech_recognition as sr 
+import nltk
+
+spEng = pyttsx3.init()
+spEng.setProperty('rate', 150)
+
+recognizer = sr.Recognizer()
+
+with sr.Microphone() as mic:
+    print('Say:', end=' ')
+    audio = recognizer.listen(mic, timeout=1, phrase_time_limit=5)
+    try:
+        text = recognizer.recognize_google(audio)
+        print(text)
+        spEng.say(text)
+        spEng.runAndWait
+    except Exception as err:
+        print('\nCould not recognise')
